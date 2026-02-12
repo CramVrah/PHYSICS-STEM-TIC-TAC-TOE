0ef2ea116ea1de83dd91aac0f2141f17e66069cd
CramVrah <mhabunyi@gmail.com>
2026-02-11 20:35:34 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE = "physics-ttt-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/questions.js",
+  "/manifest.webmanifest",
+  "/apple-touch-icon.png",
+  "/icon-72x72.png",
+  "/icon-96x96.png",
+  "/icon-128x128.png",
+  "/icon-144x144.png",
+  "/icon-152x152.png",
+  "/icon-192x192.png",
+  "/icon-384x384.png",
+  "/icon-512x512.png",
+  "/icon.svg",
+  "/mario-bgm.mp3",
+  "/correct.wav",
+  "/buzzer.wav"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(req, copy));
+          return res;
+        })
+        .catch(() => caches.match("/index.html"));
+    })
+  );
+});
